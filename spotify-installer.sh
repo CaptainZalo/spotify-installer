a090c37804b165c4aed8012df6c02812cdcf65a9
CaptainZalo <captzalo@gmail.com>
2019-09-13 12:22:32 +0200

    Update spotify-installer.sh
    Cleaned up the script a bit.

--- spotify-installer.sh
+++ spotify-installer.sh
@@ -6,16 +6,14 @@ if ! hash apt-get 2>/dev/null; then
         echo "Sorry. This script requires a distribution with Synaptic package manager (Debian distributions)."
         exit 1
 fi
-# Check if script is ran in root context.
+# Check if script is running in root context.
 if [ "$(id -u)" -ne "0" ]; then
         echo "This script requires root."
         exit 1
 fi
 # 1. Add the Spotify repository signing key to be able to verify downloaded packages
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys BBEBDCB318AD50EC6865090613B00F1FD2C19886
+curl -sS https://download.spotify.com/debian/pubkey.gpg | sudo apt-key add -
 # 2. Add the Spotify repository
-echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
-# 3. Update list of available packages
-sudo apt-get -y update
-# 4. Install Spotify
-sudo apt-get -y install spotify-client
+echo deb "http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
+# 3. Update list of available packages and install Spotify
+sudo apt-get update && sudo apt-get -y install spotify-client -noauthentication
