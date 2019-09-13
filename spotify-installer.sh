8ab2aa72dd8fcefd458f850af01ba912c6172171
CaptainZalo <captzalo@gmail.com>
2019-09-13 12:24:36 +0200

    Update spotify-installer.sh
    Fixed a typo.

--- spotify-installer.sh
+++ spotify-installer.sh
@@ -16,4 +16,4 @@ curl -sS https://download.spotify.com/debian/pubkey.gpg | sudo apt-key add -
 # 2. Add the Spotify repository
 echo deb "http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
 # 3. Update list of available packages and install Spotify
-sudo apt-get update && sudo apt-get -y install spotify-client -noauthentication
+sudo apt-get update && sudo apt-get -y install spotify-client
